ee41da2219091711c366e7da019d1cb505907363
tencar <info@tencar.ru>
2022-07-13 00:56:25 +0300

    fix light

--- ios/Classes/IosARView.swift
+++ ios/Classes/IosARView.swift
@@ -42,7 +42,7 @@ class IosARView: NSObject, FlutterPlatformView, ARSCNViewDelegate, UIGestureReco
         binaryMessenger messenger: FlutterBinaryMessenger
     ) {
         self.sceneView = ARSCNView(frame: frame)
-        self.sceneViewautoEnablesDefaultLighting = true
+        self.sceneView.autoEnablesDefaultLighting = true
         self.coachingView = ARCoachingOverlayView(frame: frame)
         
         self.sessionManagerChannel = FlutterMethodChannel(name: "arsession_\(viewId)", binaryMessenger: messenger)
